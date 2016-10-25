9ccc056bf92ced3981a3443fd69c785d5ff12bcc
Trev <trevor.pawlewicz@gmail.com>
2016-10-25 11:53:03 -0400

    adding mongoDB database connection to app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+// require package dependencies:
 var express        = require("express");
 var app            = express();
 var bodyParser     = require("body-parser");
 var mongoose       = require("mongoose");
 var flash          = require("connect-flash"); // messages
-var passport       = require("passport");
-var LocalStrategy  = require("passport-local");
-var methodOverride = require("method-override");
-var moment         = require("moment");
+var passport       = require("passport"); // authentication
+var LocalStrategy  = require("passport-local"); // authentication
+var methodOverride = require("method-override"); //for routes
+var moment         = require("moment"); // date & time
 
-//  Schemas                  ./ = current directory
-var Bar           = require("./models/bar.js");
-var Comment       = require("./models/comment.js");
-var User          = require("./models/user.js");
+//  SCHEMAS                  ./ = current directory
+var Bar           = require("./models/bar.js");     // import model
+var Comment       = require("./models/comment.js"); // import model
+var User          = require("./models/user.js");    // import model
 
 var seedDB        = require("./seeds.js"); // for seeding the database with data
 
@@ -21,8 +22,19 @@ var commentRoutes = require("./routes/comments.js"),
     indexRoutes   = require("./routes/index.js"); // AUTHENTICATION
 //-----------------------------------------------------------------------------
 
+var PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://localhost/my_philly");
+// DATABASE:
+//mongoose.connect("mongodb://localhost/my_philly"); // localhost:3000 database
+// ------------v
+// from commandline create env. var: export DATABASE_URL=mongodb://localhost/my_philly
+// to create OUR enviornment variable "DATABASE_URL"
+var url = process.env.DATABASE_URL || 'mongodb://localhost/my_philly';
+console.log("-----> DATABASE_URL = " + process.env.DATABASE_URL);
+mongoose.connect(url);
+// Heroku env. var set at its website
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 // __dirname is the directory the app.js live in
@@ -69,8 +81,7 @@ app.use(commentRoutes);
 
 //=============================================================================
 // SERVER
-var port = process.env.PORT || 3000;
-app.listen(port, process.env.IP, function() {
-    console.log("----> myPhilly Server has started on port: " + port);
+app.listen(PORT, process.env.IP, function(req, res) {
+    console.log("----> myPhilly Server has started on port: " + PORT);
 });
 //=============================================================================
